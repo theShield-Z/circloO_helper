5017cb07bebb0003da3c09a66d17bc6cf81cce11
Shield Z <126108613+theShield-Z@users.noreply.github.com>
2025-07-11 04:00:47 -0400

    oops i forgot to change the version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='circloo_helper',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     description='Build and edit circloO levels using Python',
     author='Shield Z',
