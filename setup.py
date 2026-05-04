25c021ceb1ad813914f0812dfce19ef4034fb238
Shield Z <126108613+theShield-Z@users.noreply.github.com>
2026-05-04 01:10:08 -0400

    Update version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='circloo_helper',
-    version='1.0.1',
+    version='1.1.0',
     packages=find_packages(),
     description='Build and edit circloO levels using Python',
     author='Shield Z',
